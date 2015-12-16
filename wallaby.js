734372497c3f9a4dda36b96d4a114ff278ab5184
Jared Prather <jared.prather@centare.com>
2015-12-16 14:19:24 -0600

    Updated wallaby and webpack config to work with sinon

--- wallaby.js
+++ wallaby.js
@@ -1,6 +1,7 @@
 var wallabyWebpack = require('wallaby-webpack');
 var babel = require('babel-core');
 var webpack = require('webpack');
+var webpackConfig = require('./webpack.config.dev');
 var path = require('path');
 
 module.exports = function configWallaby(wallaby) {
@@ -12,18 +13,7 @@ module.exports = function configWallaby(wallaby) {
     presets: ['es2015', 'react']
   });
 
-  var webpackPostprocessor = wallabyWebpack({
-    plugins: [
-      new webpack.ResolverPlugin(
-          new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin('bower.json', ['main'])
-      ),
-    ],
-
-    resolve: {
-      modulesDirectories: ['node_modules', path.join(__dirname, 'node_modules'), path.join(__dirname, 'bower_components')],
-      extensions: ['', '.js', '.jsx'],
-    },
-  });
+  var webpackPostprocessor = wallabyWebpack(webpackConfig);
 
   return {
     files: [
@@ -32,16 +22,17 @@ module.exports = function configWallaby(wallaby) {
       { pattern: 'node_modules/react-tools/src/test/phantomjs-shims.js', instrument: false },
       // { pattern: 'node_modules/chai/chai.js', instrument: false},
       { pattern: 'app/**/*.js*', load: false },
+      { pattern: '!app/**/*spec.js*', load: false },
     ],
 
     tests: [
-      { pattern: 'test/**/*spec.js*', load: false },
+      { pattern: 'app/**/*spec.js*', load: false },
     ],
 
     compilers: {
       '**/*.js*': babelCompiler,
     },
-    testFramework: "mocha@2.0.1",
+    testFramework: 'mocha@2.0.1',
     postprocessor: webpackPostprocessor,
     bootstrap: function () {
       // var mocha = wallaby.testFramework;
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,16 +5,16 @@ module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
     'webpack-hot-middleware/client',
-    './app/index.jsx'
+    './app/index.jsx',
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/static/'
+    publicPath: '/static/',
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin(),
   ],
   resolve: {
     extensions: ['', '.js', '.jsx'],
@@ -23,7 +23,34 @@ module.exports = {
     loaders: [{
       test: /\.(js|jsx)$/,
       loaders: ['babel'],
-      include: path.join(__dirname, 'app')
-    }]
-  }
+      include: path.join(__dirname, 'app'),
+    }, {
+      test: /\.css$/,
+      loader: 'style!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+      include: __dirname,
+    }, {
+      test: /\.less$/,
+      loader: 'style!css!less',
+      include: __dirname,
+    }, {
+      test: /\.json$/,
+      loader: 'json-loader',
+      include: __dirname,
+    }, {
+      test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      loader: 'url-loader?limit=10000&minetype=application/font-woff',
+    },
+  ],
+  },
+  externals: {
+      'jsdom': 'window',
+      'cheerio': 'window',
+      'react/lib/ExecutionEnvironment': true,
+      'react/lib/ReactContext': true
+  },
+  node: {
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+  },
 };
