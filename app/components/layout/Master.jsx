d3caab5546f504ebd40dd480a934ad2cbb30be40
Jared Prather <pratherjared@gmail.com>
2015-12-20 16:12:25 -0600

    Add appbar and left nav to master layout

--- app/components/layout/Master.jsx
+++ app/components/layout/Master.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { AppBar, LeftNav } from 'material-ui';
 import 'normalize.css';
 
 export default class CoreLayout extends Component {
@@ -6,7 +7,22 @@ export default class CoreLayout extends Component {
     children: React.PropTypes.object.isRequired,
   }
 
+  handleToggleNavClick = () => {
+    if (this.refs.leftNav) {
+      this.refs.leftNav.toggle();
+    }
+  }
+
   render() {
-    return (<div>{this.props.children}</div>);
+    return (
+      <div>
+        <AppBar
+          title="Mark II"
+          onLeftIconButtonTouchTap={this.handleToggleNavClick}
+        />
+        <LeftNav ref="leftNav" docked={false}/>
+        {this.props.children}
+      </div>
+    );
   }
 }
