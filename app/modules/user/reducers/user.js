d0a810064847310daaced38ed77922c51f958c7f
Jared Prather <jared.prather@centare.com>
2016-02-03 17:48:58 -0600

    User reducer sets token on user sign in success

--- app/modules/user/reducers/user.js
+++ app/modules/user/reducers/user.js
@@ -1,16 +1,11 @@
 import * as types from '../constants';
 
-// const initialState = {
-//   userId: '',
-//   username: '',
-//   email: '',
-//   token: '',
-// };
-
 export default function user(state = {}, action) {
   switch (action.type) {
-    case types.USER_SIGN_IN:
-      return { username: 'asdf' };
+    case types.USER_SIGN_IN_SUCCESS:
+      return {
+        token: action.payload.token,
+      };
     case types.USER_SIGN_OUT:
       return {};
     default:
