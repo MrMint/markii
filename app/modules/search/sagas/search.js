609afccf00346083e2c12f4e7983a363c23f005f
Jared Prather <pratherjared@gmail.com>
2016-02-06 12:56:12 -0600

    Song search support for cancellation and multiple sources

--- app/modules/search/sagas/search.js
+++ app/modules/search/sagas/search.js
@@ -1,18 +1,43 @@
-import { call, take, put, race } from 'redux-saga';
+import { take, put, race, cancel, fork, join } from 'redux-saga';
 import { SEARCH_MEDIA_REQUEST } from '../constants';
-import { search } from '../../../utilities/api/youtubeApi';
+import { search as searchYoutubeApi } from '../../../utilities/api/youtubeApi';
 import { receiveMediaResults } from '../actions';
 
 function* searchYoutube(query) {
-  return yield search(query);
+  return yield searchYoutubeApi(query);
 }
 
 export function* songSearch() {
+  let query;
   /* eslint-disable */
   while(true) {
   /* eslint-enable */
-    const { payload: { query } } = yield take(SEARCH_MEDIA_REQUEST);
-    const results = yield call(searchYoutube, query);
-    yield put(receiveMediaResults(results));
+
+    // Wait for the user to execute a search
+    // If query is defined, skip as we are dealing with a cancellation
+    if (!query) {
+      ({ payload: { query } } = yield take(SEARCH_MEDIA_REQUEST));
+    }
+
+    // Start search tasks asynchronously
+    const searchYoutubeTask = yield fork(searchYoutube, query);
+    const searchTasks = [
+      searchYoutubeTask,
+    ];
+
+    // Wait for all search tasks to complete, or the user to start a new search
+    const { results, search } = yield race({
+      results: searchTasks.map(join),
+      search: take(SEARCH_MEDIA_REQUEST),
+    });
+
+    // Dispatch the results or cancel search tasks and requery
+    if (results) {
+      yield put(receiveMediaResults(results.concat.apply([], results)));
+      query = undefined;
+    } else {
+      searchTasks.forEach(cancel);
+      query = search.payload.query;
+    }
   }
 }
