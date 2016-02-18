cbaa1e040704b5c7cb2aa5329d0710ce99fd3574
Jared Prather <pratherjared@gmail.com>
2016-02-17 21:31:40 -0600

    Added user saga which fetches user details on sign in

--- app/modules/sagas.js
+++ app/modules/sagas.js
@@ -1,8 +1,10 @@
 import { fork } from 'redux-saga/effects';
 import { authentication } from './user/sagas/auth';
 import { songSearch } from './search/sagas/search';
+import { user } from './user/sagas/user';
 
 export default function* root() {
   yield fork(authentication);
   yield fork(songSearch);
+  yield fork(user);
 }
